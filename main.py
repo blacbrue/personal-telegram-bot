fb8bca9178e1ee05cc03cef61f03613671116083
Jayden Koh Jia Jian <66878523+jaydenkoh@users.noreply.github.com>
2022-03-25 22:49:42 +0800

    fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-import os
 from telebot.async_telebot import AsyncTeleBot
 import asyncio
 
@@ -18,7 +17,6 @@ def main():
     async def send(msg):
         if msg.chat.type == "private" and msg.chat.type != "group":
             print("{0} {3} ({2}) sent a message: {1}".format(msg.from_user.first_name, msg.text, msg.chat.id, msg.from_user.last_name))
-            # await jayden.send("{0} {3} ({2}) sent a message: {1}".format(msg.from_user.first_name, msg.text, msg.chat.id, msg.from_user.last_name))
             chatId = input("Who would you like to reply to (chat id): ")
             replyText = input("Your answer: ")
             if replyText == "none":
