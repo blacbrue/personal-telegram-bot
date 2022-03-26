6e949558502476716499c0caec632d89ef05a76d
Jayden Koh Jia Jian <66878523+jaydenkoh@users.noreply.github.com>
2022-03-26 12:56:02 +0800

    asssd

--- node-js-version/handlers/slashCommands.js
+++ node-js-version/handlers/slashCommands.js
@@ -10,14 +10,14 @@ module.exports = (client, bot) => {
     client.on("interactionCreate", async interaction => {
         if (interaction.isCommand()) {
             if (interaction.commandName === "send") {
-                await interaction.deferReply()
+                await interaction.deferReply({ ephemeral: true })
 
                 let chatid = interaction.options.getString("chatid")
                 let messagets = interaction.options.getString("message")
 
                 bot.sendMessage(chatid, messagets).then(interaction.followUp({ content: "Sent the message" }))
             } else if (interaction.commandName === "reply") {
-                await interaction.deferReply()
+                await interaction.deferReply({ ephemeral: true })
 
                 let chatid = interaction.options.getString("chatid")
                 let messagets = interaction.options.getString("message")
